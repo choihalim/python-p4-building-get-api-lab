d26dfb2dab538a68ce1a13e02af6b5467bb47c03
Halim Choi <hchoicareer@gmail.com>
2023-06-01 11:01:40 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,32 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.to_dict() for bakery in bakeries]
+    response = make_response(jsonify(serialized_bakeries), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    serialized_bakery = bakery.to_dict()
+    response = make_response(jsonify(serialized_bakery), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(db.desc(BakedGood.price)).all()
+    serialized_goods = [goods.to_dict() for goods in baked_goods]
+    response = make_response(jsonify(serialized_goods), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(db.desc(BakedGood.price)).first()
+    serialized_most = most_expensive.to_dict()
+    response = make_response(jsonify(serialized_most), 200)
+    return response
 
 if __name__ == '__main__':
-    app.run(port=555, debug=True)
+    app.run(port=5555, debug=True)
